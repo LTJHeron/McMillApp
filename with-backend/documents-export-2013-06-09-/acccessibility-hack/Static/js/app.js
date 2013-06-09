cb9f33bdcf6b97bc9b02b119d62ee59f78baa3f5
mostlystatic <mostlystatic@gmail.com>
2013-06-09 12:57:03 +0100

    Add backend to code.

---
+++ with-backend/documents-export-2013-06-09-/acccessibility-hack/Static/js/app.js
@@ -0,0 +1,194 @@
+$.fn.raty.defaults.path = "js/libs/raty/img";
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+    this.route("index", { path: "/" });
+    this.route("day", { path: "/day/:year/:month/:day" });
+    this.route("appointments", {path: "/appointments/:year/:month/:day"});
+});
+
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    var today = new Date();
+    //App.Router.router.transitionTo("day", [2011])
+    gotoDay(today);
+  }
+});
+
+App.DayRoute = Ember.Route.extend({
+  model: function(params) {
+    console.log(params)
+    return getDayModel(params.year, params.month, params.day)
+  }
+});
+
+App.AppointmentsRoute = Ember.Route.extend({
+    model: function(params){
+        return getDayModel(params.year, params.month, params.day)
+    }
+});
+
+App.AppointmentsController = Ember.Controller.extend({
+    addAppointment: function(){
+        addAppointment(this, {date: new Date()})
+        
+        window.location.reload();
+    },
+    gotoDayView: function(){
+        switchView("day")
+    },
+    save: function(){
+        saveModel(this.get("model"))
+    }
+});
+
+function switchView(viewName){
+    window.location.hash="/" + viewName + "/" + document.location.hash.split("/").splice(2).join("/")
+}
+
+function addAppointment(controller, data){
+        var obj = JSON.parse(JSON.stringify(controller.get("model")));
+        obj.appointments.push(data)
+        saveModel(App.DayModel.create(obj));
+
+}
+
+App.AppointmentsView = Ember.View.extend({
+    didInsertElement: function(){
+        var view = this;
+        updateHeader(view.get("controller.model.date"))
+       
+    }
+});
+
+App.DayController = Ember.Controller.extend({
+    save: function(){
+        var model = this.get("model");
+
+        saveModel(model);
+    },
+    gotoAppointmentsView: function(){
+        switchView("appointments")
+    }
+});
+
+function saveModel(model){
+    var data = JSON.parse(JSON.stringify(model));
+    console.log(data)
+    data.mood = data.moodRating;
+    console.log(data)
+    $.ajax({
+        url: window.apiRoot + "editDay",
+        type: "post",
+        data: data
+    })
+    var json = JSON.stringify(data);
+    //localStorage.setItem(new Date(model.get("date")).toISOString().split("T")[0], json)
+}
+
+App.DayView = Ember.View.extend({
+    didInsertElement: function(){
+        var view = this;
+        updateHeader(view.get("controller.model.date"))
+        $(".star-rating-editable").each(function(){
+            $(this).raty({
+                score: $(this).data("score"),
+                click: function(score){
+                    view.get("controller.model").set($(this).data("model-name"), score);
+                }
+            });
+        });
+        $(".star-rating-editable").width(200)
+    }
+});
+
+App.DayModel = Ember.Object.extend({
+    date: null,
+    moodRating: 0,
+    moodDescription: "",
+    food: "",
+    health: "",
+    fatigue: "",
+    concerns: "",
+    appointments: []
+});
+
+App.AppointmentModel =  Ember.Object.extend({
+    date: null,
+    name: "",
+    details: "",
+    notes: ""
+});
+
+window.apiRoot = "http://localhost:10098/"
+
+
+function getDayModel(year, month, day){
+    var dayString = "" + day;
+    if (day < 10){
+        dayString = "0" + dayString;
+    }
+    var monthString = "" + month;
+    if (month < 10){
+        monthString = "0" + monthString;
+    }
+    var ajaxRet = null;
+    $.ajax({
+
+         url:    window.apiRoot + "viewDay?day=" + dayString + "/" + monthString + "/" + year,
+         success: function(result) {
+                      ajaxRet = result;
+                  },
+         async:   false
+    });          
+
+    var data = JSON.parse(ajaxRet)[0]
+    data.moodRating = data.mood;
+
+    return App.DayModel.create(data);
+
+
+    console.log("getDayModel", year, month, day)
+    var json = localStorage.getItem(new Date(year, month, day).toISOString().split("T")[0]);
+    var data = json !== null ? JSON.parse(json) : {date: new Date(year, month, day)};
+
+    data.appointments = data.appointments || [];
+    for (var i=0;i<data.appointments.length;i++){
+        data.appointments[i].id=i;
+    }
+    return App.DayModel.create(data);
+}
+
+function gotoDay(date){
+    page = location.hash.split("/")[1];
+    window.location.hash = "/" + page + "/" + date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate();
+    document.location.reload()
+
+}
+
+function updateHeader(date){
+    console.log("updating header with", date)
+    var html = $("#header-template").html();
+    $("#header").html(html);
+    date = new Date(date);
+    var dateString = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()] + " " + date.getDate() + " " + date.getFullYear()
+    $("#header").find("h1").text(dateString);
+    $("#header .prev").click(function(){
+        var newDate = new Date(date);
+        newDate.setDate(newDate.getDate() - 1)
+        gotoDay(newDate)
+    })
+    $("#header .next").click(function(){
+        var newDate = new Date(date);
+        newDate.setDate(newDate.getDate() + 1)
+        gotoDay(newDate)
+    });
+}
+
+
+Ember.Handlebars.registerBoundHelper('myHelper', 
+    function(value, options) {
+        return value.toUpperCase();
+    }
+);
---
+++ with-backend/documents-export-2013-06-09-/acccessibility-hack/main.py
@@ -0,0 +1,125 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from google.appengine.dist import use_library
+use_library('django', '1.3')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+import cgi
+
+from google.appengine.ext import webapp
+from google.appengine.api import users
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+from google.appengine.ext.db import djangoforms
+from google.appengine.ext.webapp.util import login_required
+import json
+import urlparse
+import urllib
+from datetime import date
+#from flask import jsonify
+#from Models import *
+
+class Treatment(db.Model):
+    name =db.StringProperty()
+    details =db.StringProperty()
+    notes =db.StringProperty()
+    
+class Medication(db.Model):
+    name = db.StringProperty()
+    reason = db.StringProperty()
+    instructions = db.StringProperty()
+    dosage = db.StringProperty()
+    repeat_prescription = db.BooleanProperty()
+
+class Activity(db.Model):
+    name = db.StringProperty()
+    duration = db.IntegerProperty()
+    intensity = db.StringProperty()
+    notes = db.StringProperty()
+class Day(db.Model):
+    date = db.StringProperty()
+    fatigue = db.IntegerProperty()
+    treatments = db.ReferenceProperty(Treatment)
+    mood = db.IntegerProperty()
+    mood_detail = db.StringProperty()
+    medication = db.ListProperty(db.Key)
+    food = db.StringProperty()
+    symptoms = db.StringProperty()
+    concerns = db.StringProperty()
+    activity = db.ReferenceProperty(Activity)
+class CalendarEntry(djangoforms.ModelForm):
+    class Meta:
+        model = Day
+        exclude = ['treatments', 'medication']
+        
+class MakeCalendarEntry(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('<html><body>'
+                                '<form method="POST" '
+                                'action="/editDay">'
+                                '<table>')
+        self.response.out.write(CalendarEntry())
+        self.response.out.write('</table>''<br>'
+                                '<input type="submit">'
+                                '</form></body></html>')
+
+    def post(self):
+        url = self.request.url
+        o=urlparse.urlparse(url)
+        #dates = urlparse.parse_qs(o.query)['day']
+        #cal_date = dates.pop(0)
+        data = CalendarEntry(data=self.request.POST)
+        if data.is_valid():
+            entity =data.save(commit=False)
+            #parts = cal_date.split('/')
+            #entity.date = cal_date #date(parts[2],parts[1], parts[0])
+            entity.put()
+            
+        else:
+            print "Error:"
+  
+class ViewCalendarEntry(webapp.RequestHandler):
+    def get(self):
+        url = self.request.url
+        o=urlparse.urlparse(url)
+        dates = urlparse.parse_qs(o.query)['day']
+        cal_date = dates.pop(0)
+        #for key, value in date.iteritems():
+        #self.response.out.write("cal: " + cal_date)
+        #date = self.request.get('da.y')
+        #day = Day.all()
+        #q = day.get()
+        #self.response.out.write(q.date)
+        day = db.GqlQuery("SELECT * FROM Day WHERE date = :1", cal_date)
+        query_result = {}
+        index =0
+        for q in day.run():
+            json_out = json.JSONEncoder().encode([dict(fatigue=q.fatigue, mood = q.mood, date=q.date)])
+            #self.response.out.write(json_out)
+            query_result[index] = json_out
+            index = index+1
+        self.response.out.write(json_out)
+        #return self.redirect('view.html?%s'% urllib.urlencode(query_result))
+        
+class MainPage(webapp.RequestHandler):
+    
+    
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('A Cancer Diary')
+
+
+app = webapp.WSGIApplication([('/editDay', MakeCalendarEntry),
+                                       ('/viewDay', ViewCalendarEntry),], debug=True)
+
+
+def main():
+    ##application = django.core.handlers.wsgi.WSGIHandler()
+
+  # Run the WSGI CGI handler with that application.
+    #util.run_wsgi_app(application)
+    run_wsgi_app(app)
+
+if __name__ == "__main__":
+    main()
